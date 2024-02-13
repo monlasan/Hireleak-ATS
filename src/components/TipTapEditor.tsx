3d1dcd65a0501d23acf4629584b21dbd658e05bc
Khaled Sanny <khaledsannyaml@gmail.com>
2024-02-13 15:08:41 +0100

    fix: fixed layout shift when submitting a tiptap editor input

--- src/components/TipTapEditor.tsx
+++ src/components/TipTapEditor.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import TipTapEditorCommands from './TipTapEditorCommands';
+import { cn } from '@/lib/utils';
 type Props = {
   isSubmitting?: boolean;
   editorText: string;
@@ -19,8 +20,13 @@ const TiptapEditor = ({ isSubmitting, editorText, setEditorText }: Props) => {
 
   return (
     <>
-      {editor && !isSubmitting ? (
-        <div className='border rounded'>
+      {editor ? (
+        <div
+          className={cn(
+            'border rounded',
+            isSubmitting && 'opacity-50 select-none pointer-events-none'
+          )}
+        >
           <TipTapEditorCommands editor={editor} />
           <div className='p-2 outline-none px-4 prose prose-h1:my-1 prose-h2:my-1 prose-h3:my-1 prose-h4::my-1 prose-h5:my-1 prose-h6:my-1 prose-p:my-1 prose-ul:my-1 prose-ol:my-1 prose-blockquote:my-1'>
             <EditorContent editor={editor} />
@@ -30,8 +36,6 @@ const TiptapEditor = ({ isSubmitting, editorText, setEditorText }: Props) => {
         <div className='border rounded animate-pulse'>
           <div className='flex flex-wrap h-[49px] border-b bg-background text-foreground'></div>
           <div className='h-[120px] outline-none bg-background'></div>
-          {/* <div className='h-[120px] hover:h-[420px] outline-none bg-background'></div> */}
-          {/* <div className='h-[120px] hover:h-[420px] transition-[height_0.4s] outline-none bg-background'></div> */}
         </div>
       )}
     </>
